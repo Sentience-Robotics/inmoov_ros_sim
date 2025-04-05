2eb3889ae7e603126de65bd8ddb5465739c37b90
m-brl <mathieu.borel@epitech.eu>
2025-04-05 12:38:44 +0200

    wip(wip): wip
    Signed-off-by: m-brl <mathieu.borel@epitech.eu>

--- scripts/docker_install.sh
+++ scripts/docker_install.sh
@@ -12,6 +12,6 @@ fi
 $DOCKER_COMPOSE -f docker-compose.yml up -d
 
 echo -e "\033[32m\033[1mInstallation complete\033[0m, you can now access the container with the following command:"
-echo "chmod -R 777 inmoov_ros_sim && \"
-echo "cd inmoov_ros_sim && \"
+echo "chmod -R 777 inmoov_ros_sim && \\"
+echo "cd inmoov_ros_sim && \\"
 echo "make dc-term"
